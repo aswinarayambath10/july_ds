b4ccc97972e6f2a7cd40557371281658d9bef482
aswin <aswinarayambath10@gmail.com>
2025-12-10 13:29:12 +0530

    work2 modif

---
+++ work2.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
+def add(x, y):
+    return x + y
